38b7a32d40a899cd5bfe67d4367445642f283ba3
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2021-12-02 15:43:27 -0300

    Initial Commit

---
+++ Planejador/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Planejador
+//
+//  Created by Leonardo de Sousa Rodrigues on 02/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Planejador/PlanejadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanejadorApp.swift
+//  Planejador
+//
+//  Created by Leonardo de Sousa Rodrigues on 02/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PlanejadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
